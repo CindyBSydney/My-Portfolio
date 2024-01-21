e8d78b38c6d758b9363ea91f59fcdce89cf6d0e4
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-01-21 15:42:33 +0300

    feat(navigation): add interactive functionality
    Added code to toggle the navigation menu's visibility.
    Added event listeners to the menu button and navigation items for interactive behavior.
    Clicking on a navigation item closes the menu.

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ window.addEventListener("scroll", function() {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+//navigation menu
+const menuBtn = document.querySelector(".menu-button");
+const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelectorAll(".navigation a");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+navigationItems.forEach((navigationItem) => {
+    navigationItem.addEventListener("click", () => {
+        menuBtn.classList.remove("active");
+        navigation.classList.remove("active");
+    });
+});
+
